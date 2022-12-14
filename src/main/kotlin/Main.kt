d716b3a20f73f344271e7efd0d08741ed3339af3
Ilya Brant <detroitib31@gmail.com>
2022-12-14 17:18:25 +0600

    Написал цикл для заполнения массива и вывода его данных

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,10 @@ fun main(args: Array<String>) {
     var min = 0
     var max = 0
     var sum = 0
+
+    for (i in 0..12) {
+        a[i] = Random(System.nanoTime()).nextInt(0, 101)
+        print("${a[i]}, ")
+    }
+    println()
 }
